479c9cfed13d6d3362d5a81d03601a176225655e
Gillian Goud <gillian.goud@hva.nl>
2019-04-22 11:47:18 +0200

    added rbac to organizations

--- apps/api/src/app/organizations/organizations.controller.ts
+++ apps/api/src/app/organizations/organizations.controller.ts
@@ -1,41 +1,89 @@
-import { Controller, Get, Post, UseGuards, Body, Put, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Body,
+  Put,
+  Param
+} from '@nestjs/common';
 import { Organization } from './organizations.interface';
 import { OrganizationsService } from './organizations.service';
 import { AuthGuard } from '@nestjs/passport';
 import { User } from '../utilities/user.decorator';
 import { User as UserDocument, CreateOrgDTO } from '@goud-sport/api-interface';
+import { ACGuard, UseRoles } from 'nest-access-control';
 
 @Controller('organizations')
 export class OrganizationsController {
   constructor(private organizationsService: OrganizationsService) {}
 
   @Get()
+  @UseGuards(AuthGuard, ACGuard)
+  @UseRoles({
+    resource: 'organization',
+    action: 'read',
+    possession: 'any'
+  })
   async listAll(): Promise<Organization[]> {
     return await this.organizationsService.findAll();
   }
 
+  @Get('mine')
+  @UseGuards(AuthGuard, ACGuard)
+  @UseRoles({
+    resource: 'organization',
+    action: 'read',
+    possession: 'own'
+  })
+  async listOwn(): Promise<Organization[]> {
+    // TODO return all organizations a User belongs to
+    return await this.organizationsService.findAll();
+  }
+
   @Post()
-  @UseGuards(AuthGuard())
+  @UseGuards(AuthGuard, ACGuard)
+  @UseRoles({
+    resource: 'organization',
+    action: 'create',
+    possession: 'own'
+  })
   async create(
     @Body() org: CreateOrgDTO,
-    @User() user: UserDocument,
+    @User() user: UserDocument
   ): Promise<Organization> {
     return await this.organizationsService.create(org, user);
   }
 
   @Put(':id')
-  @UseGuards(AuthGuard())
-  async update(
+  @UseGuards(AuthGuard, ACGuard)
+  @UseRoles({
+    resource: 'organization',
+    action: 'update',
+    possession: 'own'
+  })
+  async updateOwn(
     @Param('id') id: string,
     @Body() org: Partial<CreateOrgDTO>,
-    @User() user: UserDocument,
+    @User() user: UserDocument
   ): Promise<Organization> {
     const { id: userId } = user;
-    if (user.admin){
-      return await this.organizationsService.update(id, org);
-    } else {
-      return await this.organizationsService.update(id, org, userId);
-    }
+    return await this.organizationsService.update(id, org, userId);
+  }
+
+  @Put(':id')
+  // TODO use a different endpoint here, idk what yet tho
+  @UseGuards(AuthGuard, ACGuard)
+  @UseRoles({
+    resource: 'organization',
+    action: 'update',
+    possession: 'any'
+  })
+  async updateAny(
+    @Param('id') id: string,
+    @Body() org: Partial<CreateOrgDTO>
+  ): Promise<Organization> {
+    return await this.organizationsService.update(id, org);
   }
 
   // TODO return a joinDTO with confirmation instead of the whole org object
@@ -44,11 +92,9 @@ export class OrganizationsController {
   async joinOrg(
     @Param('id') id: string,
     @Body() userDocument: Partial<UserDocument>,
-    @User() user: UserDocument,
+    @User() user: UserDocument
   ): Promise<Organization> {
     const { id: userId } = user;
     return await this.organizationsService.addMember(id, userDocument, userId);
   }
-
-
 }
